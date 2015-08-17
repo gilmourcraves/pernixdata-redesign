ae1897c70f73a5950fc112518b714ec9a32adcb2
pelicanpaul <paullyons@pacbell.net>
2015-08-17 16:44:09 -0700

    buildout of version 2 - pretty fancy stuff!!!

--- js/redesign2.js
+++ js/redesign2.js
@@ -1,5 +1,28 @@
 
+
+
 jQuery(document).ready(function($){
+
+
+
+
+
+$(window).on('scroll', function(){
+    // Loop over each container, and check if it's visible.
+    $('.region-parallax').each(function(){
+        var visible = $(this).visible();
+        var circleId = $(this).data('circleid');
+
+        //console.log(visible);
+
+        if(visible == true){
+            $('.circle').removeClass('circle-on');
+            $('[data-id="' + circleId +'"]').addClass('circle-on');
+        }
+    });
+});
+
+
     $('.slider-home').slick({
             autoplay: false,
             arrows: true,
@@ -11,20 +34,49 @@ jQuery(document).ready(function($){
 
     var windowHeight = $(window).height();
     var windowWidth = $(window).width();
+    var centerH1 = ((windowHeight - 120) / 2) - 115;
+    var contentWidth =  1109;
+    var arrowsLR = ((windowWidth - contentWidth) / 2) -100;
+    var gotoNav = ((windowWidth - contentWidth) / 2) -200;
+
 
     if(windowHeight > 600) {
-        $('.region-01').css('height', (windowHeight - 140) + 'px');
+        $('.region-01').css('height', (windowHeight - 120) + 'px');
+        $('.slider-home, .slider-home-item').css('height', (windowHeight - 120) + 'px').css('width', (windowWidth) + 'px');
+        $('.slider-home-center').css('margin-top', centerH1 + 'px');
+        $('#right-utility').css('top', centerH1 + 140 + 'px');
+        $('#right-utility-nav').css('top', centerH1 + 140 + 'px').css('right', gotoNav + 'px');
+        $('.slick-prev').css('left', arrowsLR + 'px');
+        $('.slick-next').css('right', arrowsLR + 'px');
     }
 
-    $('.slider-home, .slider-home-item').css('height', (windowHeight - 140) + 'px');
-    $('.slider-home,  .slider-home-item').css('width', (windowWidth) + 'px');
+    $('.circle').click(function(){
+        var goto = $(this).data('target');
+        var gotoTarget = $(goto);
+        var gotoOffset = 160;
+
+        if(goto == '.region-01'){
+            gotoOffset = 120
+        }
+
+        $('.circle').removeClass('circle-on');
+
+        $(this).addClass('circle-on');
+
+        $('html,body').animate({scrollTop: gotoTarget.offset().top - gotoOffset});
+    });
+
+
+
+
+
 
     $('.slider-home-customers').slick({
             autoplay: false,
             arrows: true,
             dots: false,
-            slidesToShow: 4,
-            slidesToScroll: 4,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             mobileFirst: true,
 
             responsive: [
@@ -41,7 +93,6 @@ jQuery(document).ready(function($){
 
         }
     );
-
 
 
 });
